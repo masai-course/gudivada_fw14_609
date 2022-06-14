113d06fbe48f7d59002d544c2efa10ad16ef02d8
rameshgudivada9 <96101948+rameshgudivada9@users.noreply.github.com>
2022-06-14 20:23:13 +0530

    delete

--- rm101_assign1/src/App.js
+++ rm101_assign1/src/App.js
@@ -4,21 +4,15 @@ import "./App.css";
 function App() {
 
   const [sum,setSum]=useState(0);
-  if(sum===-1){
-    setSum(0);
-}
-   
-
   return (
     <div className="App">
-      <h2 data-testid="counter-value">COUNTER APP</h2>
       <h2 data-testid="counter-value" className="sum">{sum}</h2>
       <button data-testid="counter-decrement-button" className="decre" onClick={()=>{
         setSum(sum-1);
-      }} disabled={sum===0 ? true : false} >Decrement by 1</button>
+      }}
+       disabled={sum===0 ? true : false} >Decrement by 1</button>
       <button data-testid="counter-increment-button" className="incre" onClick={()=>{
         setSum(sum+1);
-        // setSum(-1);
       }}
       >Increment by 1</button>
     </div>
