7f5cb642d5f7a212b0a6772ca6b0cd6f3d8a6a4a
rameshgudivada9 <96101948+rameshgudivada9@users.noreply.github.com>
2022-06-14 19:56:49 +0530

    adding code

--- rm101_assign1/src/App.js
+++ rm101_assign1/src/App.js
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+
+  const [sum,setSum]=useState(0);
+  if(sum===-1){
+    setSum(0);
+}
+   
+
   return (
     <div className="App">
-      <h2 data-testid="counter-value">X</h2>
-      <button data-testid="counter-decrement-button"></button>
-      <button data-testid="counter-increment-button"></button>
+      <h2 data-testid="counter-value">{sum}</h2>
+      <button data-testid="counter-decrement-button" onClick={()=>{
+        setSum(sum+1);
+      }} >increse by 1</button>
+      <button data-testid="counter-increment-button" onClick={()=>{
+        setSum(sum-1);
+        setSum(-1);
+      }}
+      disabled={sum===0 ? true : false} >decrese by 1</button>
     </div>
   );
 }
